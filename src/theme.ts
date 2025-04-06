59cf9cf30f98b458c48770b5774dd6215b21444a
RayKay91 <61206305+RayKay91@users.noreply.github.com>
2025-04-06 19:23:26 +0100

    feat: create basic theme colour palette from medexpress website

---
+++ src/theme.ts
@@ -0,0 +1,11 @@
+export const theme = {
+  light: {
+    primaryLight: 'rgb(193,211,241)',
+    primaryRegular: 'rgb(42, 81, 138)',
+    primaryDark: 'rgb(23, 62, 125)',
+    secondaryRegular: 'rgb(237, 138, 62)',
+    surfaceBase: 'rgb(255, 255, 255)',
+    surfaceRegular: 'rgb(238,240,249)',
+  },
+  // @todo implement dark mode theme dark: {},
+};
